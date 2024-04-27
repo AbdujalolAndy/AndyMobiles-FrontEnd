81719a9eec94aa03e8ee5316d3a5ed2d062d3d05
AbdujalolAndy <mrarnoldunlimited@gmail.com>
2024-04-27 15:31:26 +0900

    fix: Deploy execubale

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,10 @@ git reset --hard
 git pull origin master
 
 npm i yarn -g
+yarn global add serve
 yarn
 yarn run build
-pm2 start "serve -s build" --name=ANDYMOBILE-REACT
+pm2 start "yarn run start:prod" --name=ANDYMOBILE-REACT
 
 # DEVELOPMENT
 # npm i yarn -g
