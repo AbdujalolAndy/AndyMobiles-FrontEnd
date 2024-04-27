c723730a2ed6221b390a4b551edfbdc712b43c5d
AbdujalolAndy <mrarnoldunlimited@gmail.com>
2024-04-27 11:52:00 +0900

    feat: Pm2 deploy logic code

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+# !bin/bash
+
+# PRODUCTION
+git reset --hard
+git pull origin master
+
+npm i yarn -g
+yarn
+yarn run build
+pm2 start "yarn run start:prod" --name=ANDYMOBILE-REACT
+
+# DEVELOPMENT
+# npm i yarn -g
+# pm2 start "yarn run start" --name=ANDYMOBILE-REACT
